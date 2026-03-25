451a581b00da523b2c9f3133f7f7601dd18b0177
DanielTrujillo5 <danieltigo555@gmail.com>
2026-03-25 07:37:28 -0500

    primer punto

---
+++ academy_dt/src/main/java/com/academy/academy_dt/AcademyDtApplication.java
@@ -0,0 +1,13 @@
+package com.academy.academy_dt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyDtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyDtApplication.class, args);
+	}
+
+}
---
+++ academy_dt/src/test/java/com/academy/academy_dt/AcademyDtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.academy_dt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyDtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
